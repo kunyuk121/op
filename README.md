ae1bf454c1dee20da2095354a8ba58c2eb9075f1
0xOneiros <154099755+0xOneiros@users.noreply.github.com>
2023-12-17 16:14:06 -0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,106 @@
+## EVM Chain Compatible Inscription Automation Mint Scripts
+
+
+## 🛠 Instructions for use
+
+
+### Step 1: Install nodejs first.
+
+
+First, go to the official Nodejs website to download and install the corresponding version for your computer's operating system.
+
+
+```bash
+https://nodejs.org/en
+``
+
+
+Then check the installed version to see if the installation was successful.
+
+
+```bash
+node -v
+npm -v
+```
+
+
+If you prefer to use yarn, install yarn
+```bash
+npm i -g yarn
+```
+
+
+### Step 2: Downloading the script source code
+First, use git to clone the source code locally.
+```bash
+git clone https://github.com/sfter/evm-inscription-mint.git
+
+
+cd evm-inscription-mint
+```
+If you don't have git installed on your Windows computer, go to the following website to download and install the git software
+```bash
+https://gitforwindows.org
+```
+
+
+### Step 3: Rename config.js.example in the current directory to config.js.
+```bash
+cp config.js.example config.js
+```
+
+
+### Step 4: Modify the config.js configuration file in the current directory.
+```javascript
+const config = {
+    // Set the number of hits you want to make here. It's recommended that you don't make more than 50 hits at a time, or else you'll be unlinked.
+    repeatCount: 1, // Add more cards to the current gas count.
+
+
+    // How many times you want to increase the current gas.
+    increaseGas: 1.2, // The private key of your wallet.
+
+
+    // Your wallet's private key
+    privateKey: "", // the private key of your wallet
+
+
+    // Inscription json data (replace it with the json format data of the inscription you want to play)
+    tokenJson: 'data:,{"p": "fair-20", "op": "mint", "tick": "fair", "amt": "1000"}', // RPC node (compatible with RPCs)
+
+
+    // RPC nodes (evm-compatible chains are fine) use the node address of whichever chain you're playing on
+    // eth => https://mainnet.infura.io/v3
+    // arb => https://arb1.arbitrum.io/rpc
+    // polygon => https://polygon-rpc.com
+    // op => https://mainnet.optimism.io
+    // linea => https://mainnet.infura.io/v3
+    // scroll => https://rpc.scroll.io
+    // zks => https://mainnet.era.zksync.io
+    rpcUrl: "https://arb1.arbitrum.io/rpc"
+}
+```
+
+
+### Step 5: Install the dependencies
+```bash
+npm i
+npm i
+or
+``bash
+yarn install
+```
+
+
+### Step 6: Run the Mint script program
+```shell
+node index.js
+```
+or
+``shell
+yarn start
+``
+or
+``shell
+npm run start
+```
---
+++ index.js
@@ -0,0 +1,106 @@
+const { ethers } = require("ethers");
+const config = require("./config")
+
+// Connect to the node
+const provider = new ethers.providers.JsonRpcProvider(config.rpcUrl);
+
+// Create a wallet
+const wallet = new ethers.Wallet(config.privateKey.trim(), provider);
+
+async function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+// Convert to hexadecimal
+const convertToHexa = (str = '') =>{
+   const res = [];
+   const { length: len } = str;
+   for (let n = 0, l = len; n < l; n ++) {
+      const hex = Number(str.charCodeAt(n)).toString(16);
+      res.push(hex);
+   };
+   return `0x${res.join('')}`;
+}
+
+// Get the current account's nonce
+async function getCurrentNonce(wallet) {
+  try {
+    const nonce = await wallet.getTransactionCount("pending");
+    console.log("Nonce:", nonce);
+    return nonce;
+  } catch (error) {
+    console.error("Error fetching nonce:", error.message);
+    throw error;
+  }
+}
+
+// Get the current price of gas on the mainnet
+async function getGasPrice() {
+  const gasPrice = await provider.getGasPrice();
+  return gasPrice;
+}
+
+// Get real-time gasLimit on the chain
+async function getGasLimit(hexData, address) {
+  const gasLimit = await provider.estimateGas({
+    to: address,
+    value: ethers.utils.parseEther("0"),
+    data: hexData,
+  });
+
+  return gasLimit.toNumber();
+}
+
+// Transfer transactions
+async function sendTransaction(nonce) {
+  const hexData	= convertToHexa(config.tokenJson.trim());
+  // Get real-time gasPrice
+  const currentGasPrice = await getGasPrice();
+  // Increase the current gasPrice by a certain amount.
+  const gasMultiple = parseInt(String(config.increaseGas * 100))
+  const increasedGasPrice = currentGasPrice.div(100).mul(gasMultiple);
+  // Get the wallet address
+  let address = await wallet.getAddress();
+  if (config.receiveAddress !== "") {
+    address = config.receiveAddress;
+  }
+  // Get the current gasLimit limit
+  const gasLimit = await getGasLimit(hexData, address);
+  // Amount paid
+  const payPrice = config.payPrice
+
+  const transaction = {
+    to: address,
+	// Replace it with the amount you want to transfer
+    value: ethers.utils.parseEther(payPrice),
+    // Hexadecimal data
+    data: hexData,
+    // Set the nonce
+    nonce: nonce,
+    // Setting the gas price
+    gasPrice: increasedGasPrice,
+	// Limit gasLimit, based on the current network transfer settings, if you don't know how much to set go to the block browser to see how much others have transferred successfully
+    gasLimit: gasLimit,
+  };
+
+  try {
+    const tx = await wallet.sendTransaction(transaction);
+    console.log(`Transaction with nonce ${nonce} hash:`, tx.hash);
+  } catch (error) {
+    console.error(`Error in transaction with nonce ${nonce}:`, error.message);
+  }
+}
+
+// Send multiple transactions
+async function sendTransactions() {
+  const currentNonce = await getCurrentNonce(wallet);
+  const sleepTime = config.sleepTime
+
+  for (let i = 0; i < config.repeatCount; i++) {
+    const gasPrice = await getGasPrice();
+    await sendTransaction(currentNonce + i, gasPrice);
+    await sleep(sleepTime)
+  }
+}
+
+sendTransactions();
